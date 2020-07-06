02bd81f7a43f2e50dff1ad9ad64799b5c3b9cf12
ReenyeLima <52282158+ReenyeLima@users.noreply.github.com>
2020-07-06 10:03:49 -0300

    Protected

--- nota/js/JavaScript.js
+++ nota/js/JavaScript.js
@@ -1,12 +1,1 @@
-﻿< script >
-    $(function () {
-        var nav = $('#nav');
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 150) {
-                nav.addClass("view");
-            } else {
-                nav.removeClass("view");
-            }
-        });
-    });
-	</script >
+﻿
